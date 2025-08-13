d635bc98f437717f96dfb501895588c0aca9ca7c
ZenChris <121231422+ZenChris18@users.noreply.github.com>
2025-08-13 12:42:16 +0800

    added random color function

--- index.js
+++ index.js
@@ -14,10 +14,18 @@ function createGrid(gridSize) {
         container.appendChild(square)
 
         square.addEventListener('mouseenter', () => {
-            square.style.backgroundColor = 'red'
+            square.style.backgroundColor = getRandomColor()
         })
     }
 }
+
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256)
+  const g = Math.floor(Math.random() * 256)
+  const b = Math.floor(Math.random() * 256)
+  return `rgb(${r}, ${g}, ${b})`
+}
+
 
 // Create 16x16 grid on page load
 createGrid(16)
